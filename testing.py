00237dbe2aad731f14f9a2e897d1f4ed820541ec
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-27 17:16:03 -0300

    test that testing is working

--- testing.py
+++ testing.py
@@ -8,6 +8,6 @@ class TestAskMe(unittest.TestCase):
         pass
     def test_page(self):
         response = self.app.get("/", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
     def test_num_docs(self):
         pass
