f4a56665ee25b5e0c5ff2b796690a37d001754bf
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-27 17:45:43 -0300

    Move port from procfile to web

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+from os import environ
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,4 @@ def index():
     return("Hello, World!")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=environ.get("PORT", 5000))
