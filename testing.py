38bf677a4e1c336e955c2c1e316abc368b0c4856
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-27 17:19:54 -0300

    update testing file

--- testing.py
+++ testing.py
@@ -1,13 +1,16 @@
 import unittest
 from web import app
 
-class TestAskMe(unittest.TestCase):
-    def setUp(self):
+class CheckResponse(unittest.TestCase):
+    def __init__(self):
         self.app = app.test_client()
-    def tearDown(self):
-        pass
+    """def tearDown(self):
+        pass"""
     def test_page(self):
         response = self.app.get("/", follow_redirects=True)
-        self.assertEqual(response.status_code, 400)
-    def test_num_docs(self):
-        pass
+        self.assertEqual(response.status_code, 200)
+    """def test_num_docs(self):
+        pass"""
+
+if __name__ == '__main__':
+    unittest.main()
