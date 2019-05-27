90b1d7301ca03b3cd852c5c342c775039c323b62
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-27 17:35:36 -0300

    Add gitignore and fix testing

--- testing.py
+++ testing.py
@@ -2,15 +2,11 @@ import unittest
 from web import app
 
 class CheckResponse(unittest.TestCase):
-    def __init__(self):
+    def setUp(self):
         self.app = app.test_client()
-    """def tearDown(self):
-        pass"""
     def test_page(self):
         response = self.app.get("/", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-    """def test_num_docs(self):
-        pass"""
 
 if __name__ == '__main__':
     unittest.main()
