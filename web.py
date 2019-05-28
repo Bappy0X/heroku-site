49df1ecb4d353ed8f5f5a29df02551425c7a3b75
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-28 12:33:55 +0100

    Push working stuff

--- web.py
+++ web.py
@@ -2,9 +2,9 @@ from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/hello/<user>")
-def hello_name(user):
-    return render_template("hello.html", name=user)
+@app.route("/<user>")
+def index(user):
+    return render_template("index.html", name=user)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
