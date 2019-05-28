102af147b32ab820314f7347e7b44978577ac03b
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-28 17:26:58 +0100

    Publish hello world app

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
 from os import environ
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/<user>")
-def index(user):
-    return render_template("index.html", name=user)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
