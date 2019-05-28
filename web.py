246c5b311c51e696a0fa7ceb03b3b6f0e703d978
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-28 12:21:09 +0100

    Thinking emote

--- web.py
+++ web.py
@@ -2,9 +2,9 @@ from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return(render_template("pages/index.html"))
+@app.route("/<user>")
+def index(user):
+    return render_template("hello.html", name=user)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
