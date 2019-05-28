fe04d2add722f0df90bf6b0f40a36ffc44e709f9
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-28 12:24:13 +0100

    again

--- web.py
+++ web.py
@@ -2,8 +2,8 @@ from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/<user>")
-def index(user):
+@app.route("/hello/<user>")
+def hello_name(user):
     return render_template("hello.html", name=user)
 
 if __name__ == "__main__":
