12d60215c9137c6dcc3f3c90db0ad0f5cd475ae2
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-29 22:56:41 +0100

    cool

---
+++ sql/models.py
@@ -0,0 +1,5 @@
+from web import db
+
+class comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
--- web.py
+++ web.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template, Blueprint
+from os import environ
+from flask import Flask, Blueprint, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3" #environ["DATABASE_URI"]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-from views import main
+from models import *
+
+# MAIN
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def index():
+    return render_template("pages/index.html")
+
+@main.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+@main.route("/contact")
+def contact():
+    return render_template("pages/contact.html", contacts=comment.query.all())
+
+@main.route("/contact", methods=["POST"])
+def contact_post():
+    firstname = request.form.get("firstname")
+    new_comment = comment(name=firstname)
+    db.session.add(new_comment)
+    db.session.commit()
+    return redirect(url_for("main.index"))
+
 app.register_blueprint(main)
 
-"""errors = Blueprint("errors", __name__)
+# ERRORS
+errors = Blueprint("errors", __name__)
 
 @errors.errorhandler(403)
 def not_found(error):
@@ -19,7 +49,7 @@ def forbidden(error):
 def internal_server_error(error):
     return render_template("pages/error.html", code=500, error=error), 500
 
-app.register_blueprint(errors)"""
+app.register_blueprint(errors)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
