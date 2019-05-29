5f3e1a87f10cf8abd4b7105ddbe3d8d8482e46c5
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-29 11:30:44 +0100

    Gunicorn testing

--- web.py
+++ web.py
@@ -1,10 +1,28 @@
-from os import environ
-from flask import Flask, render_template, request
-app = Flask(__name__)
+from flask import Flask, render_template, Blueprint
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+def create_app():
+    app = Flask(__name__)
+
+    from views import main
+    app.register_blueprint(main)
+
+    return(app)
+
+"""errors = Blueprint("errors", __name__)
+
+@errors.errorhandler(403)
+def not_found(error):
+    return render_template("pages/error.html", code=403, error=error), 403
+
+@errors.errorhandler(404)
+def forbidden(error):
+    return render_template("pages/error.html", code=404, error=error), 404
+
+@errors.errorhandler(500)
+def internal_server_error(error):
+    return render_template("pages/error.html", code=500, error=error), 500
+
+app.register_blueprint(errors)"""
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    create_app().run(host="0.0.0.0", port=3000, debug=True)
