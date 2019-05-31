c36075412f7ad71de39eeb08e91459476aa0399e
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2019-05-31 11:15:44 +0100

    Fix testing file

--- testing.py
+++ testing.py
@@ -1,11 +1,13 @@
 import unittest
-from web import app
+from web import create_app
+
+app = create_app()
 
 class CheckResponse(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
-    def test_page(self):
-        response = self.app.get("/", follow_redirects=True)
+        self.client = app.test_client()
+    def test_index(self):
+        response = self.client.get("/", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
